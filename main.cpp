78ecd4669edea1051fde102e6452d82349c76962
VictorQuicano <alejandroxd25k@gmail.com>
2024-08-29 18:46:22 -0500

    Testing

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{   
+	system("cls");
+    cout << "\x1B[H";
+    cout << "\x1B[3;34m";
+    cout << "/***************************************************/" <<  endl; 
+	cout << "\x1B[m";
+    cout << "\x1B[31;5;88mUniversidad Nacional de San Agustin \x1B[m" <<  endl; 
+	cout << "\x1B[33;5;88mEscuela Profesional de Ciencia de la Computacion I\x1B[m" <<  endl; 
+	cout << "Curso de Estructura de Datos Avanzados" <<  endl; 
+	cout << "\x1B[38;5;46mProf. D.Sc. Manuel Eduardo Loaiza Fernandez \x1B[m" << endl; 
+	cout << "Arequipa 2023 - I" << endl; 
+	cout << "\x1B[3;34m";
+    cout << "/***************************************************/" << endl;
+	cout << "\x1B[m";
+    cout << "\nHello, World!! 2023 - Winter\n\n"; 
+
+	return 0;
+}
